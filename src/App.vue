3eb7652fac944d929e95e206baa121d9553fd43e
窪田章宏 <kubota_akihiro@kubotaakihirotaino-MacBook-Pro.local>
2022-01-01 14:58:50 +0900

    github pagesにあげる

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>GitHub Pages Sample.</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+
+
---
+++ src/pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>This is Home Page.</p>
+    <router-link to="/about">Go to About page.</router-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+
